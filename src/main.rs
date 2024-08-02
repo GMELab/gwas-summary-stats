a2435edade356804cd907d2ae9caed2fb35b7615
mrvillage <josef@mrvillage.dev>
2024-08-01 20:10:57 -0400

    Try this

--- src/main.rs
+++ src/main.rs
@@ -831,6 +831,7 @@ fn dbsnp_matching(ctx: &Ctx, raw_data: &mut Data) -> (Data, Data) {
         header: raw_data.header.clone(),
         data: raw_data_missing,
     };
+    debug!(header = raw_data.header, len = raw_data.header.len, "Raw data header");
     debug!("Reordering columns");
     let data = std::mem::take(&mut raw_data_merged.data);
     let new_data: Vec<MaybeUninit<Vec<String>>> =
